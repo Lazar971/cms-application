49bdafcbb6611eb31a64710a4d3a48d744af848e
Lazar971 <lazar.milosavljevic97@gmail.com>
2020-11-10 14:54:08 +0100

    working on docker compose

--- server/src/ormconfig.ts
+++ server/src/ormconfig.ts
@@ -4,7 +4,7 @@ import { ConnectionOptions } from "typeorm";
 
 export const config: ConnectionOptions = {
     type: "mysql",
-    host: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
     port: parseInt(process.env.DB_PORT),
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
